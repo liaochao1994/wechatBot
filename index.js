058bcf2db54f1caaffd9fc7f543337a6a2a89925
廖超 <liaochao940405@gmail.com>
2023-07-25 17:36:44 +0800

    20230725

--- index.js
+++ index.js
@@ -3,13 +3,8 @@
  *  - https://github.com/gengchen528/wechatBot
  */
 const { WechatyBuilder } = require('wechaty');
-const schedule = require('./schedule/index');
 const config = require('./config/index');
-const untils = require('./utils/index');
 const superagent = require('./superagent/index');
-
-// 延时函数，防止检测出类似机器人行为操作
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 // 二维码生成
 function onScan(qrcode, status) {
@@ -18,21 +13,28 @@ function onScan(qrcode, status) {
     'https://api.qrserver.com/v1/create-qr-code/?data=',
     encodeURIComponent(qrcode),
   ].join('');
-
   console.log(qrcodeImageUrl);
 }
 
 // 登录
 async function onLogin(user) {
   console.log(`贴心小助理${user}登录了`);
-  const date = new Date()
-  console.log(`当前容器时间:${date}`);
-  if (config.AUTOREPLY) {
-    console.log(`已开启机器人自动聊天模式`);
-  }
+  if (config.AUTOREPLY) console.log(`已开启机器人自动聊天模式`);
+  let Rooms = config.ROOMNAME;
+  for (var i = 0; i < Rooms.length; i++) {
+    try {
+      let room = await bot.Room.find({ topic: Rooms[i] }) // 获取你要发送的 - 群名称
+      // let str = '贴心小助理上线啦！\n';
+      // str += config.keywordsTip;
+      // await superagent.sendRoomMessage(room, str);
+      await superagent.roomListenJoin(room);
+    }
+    catch {
 
+    }
+  }
   // 登陆后创建定时任务
-  await initDay();
+  await superagent.initDay(bot);
 }
 
 // 登出
@@ -41,86 +43,150 @@ function onLogout(user) {
 }
 
 // 监听对话
+var reback_message_listen = true // 是否开启防撤回，默认开启
+var reback_message = [] // 自动保存最近的30条记录
 async function onMessage(msg) {
-  const contact = msg.talker(); // 发消息人
-  const content = msg.text().trim(); // 消息内容
-  const room = msg.room(); // 是否是群消息
+  const contact = await msg.talker(); // 发消息人
+  const content = await msg.text().trim(); // 消息内容
+  const room = await msg.room(); // 是否是群消息
   const alias = await contact.alias() || await contact.name(); // 发消息人备注
-  const isText = msg.type() === bot.Message.Type.Text;
-  if (msg.self()) {
-    return;
+  const isText = msg.type() === bot.Message.Type.Text; // 检测属于文字内容
+  const isImage = msg.type() === bot.Message.Type.Image; // 检测属于图片
+  // if (isImage) {
+  // let file = await msg.toFileBox() // 获取消息中的文件 - 暂时对应为图片
+  // console.log(file)
+  // }
+  let str = '';
+  if (msg.self()) return; // 如果是自己发的消息则直接过滤
+  let tianqi = content.split('天气') || []; // 天气
+  let xingzuo = content.split('运势') || []; // 运势
+  let lajifenlei = content.split('是什么垃圾') || []; // 垃圾分类
+  switch (content) {
+    case '@Bot':
+      str = config.keywordsTip;
+      break;
+    case '土味情话':
+      let low = await superagent.getSweetWord(); //获取土味情话
+      str = `${low}`;
+      break;
+    case '神回复':
+      let godReply = await superagent.getGodReply(); //获取神回复
+      str = `${godReply}`;
+      break;
+    case '拍马屁':
+      let pi = await superagent.getCaiHongPi(); //获取彩虹屁
+      str = `${pi}`;
+      break;
+    case '舔狗日记':
+      let tiangou = await superagent.getTianGou(); //获取舔狗日记
+      str = `${tiangou}`;
+      break;
+    case '毒鸡汤':
+      let du = await superagent.getDuJiTang(); //获取毒鸡汤
+      str = `${du}`;
+      break;
+    case '早上好，米娜桑':
+      let zaoan = await superagent.getZaoAn(); //获取早安心语
+      str = `${zaoan}`;
+      break;
+    case lajifenlei[0] + '是什么垃圾':
+      if (lajifenlei[0]) {
+        let fenlei = await superagent.getRubbishType(lajifenlei[0]); //获取垃圾分类
+        if (fenlei) str = `${fenlei}`;
+      }
+      break;
+    case tianqi[0] + '天气':
+      if (tianqi[0]) {
+        let weather = await superagent.getTXweather(tianqi[0]); //获取天气信息
+        if (weather) str = `${weather}`;
+      }
+      break;
+    case xingzuo[0] + '运势':
+      if (xingzuo[0]) {
+        let text = await superagent.getXingZuo(xingzuo[0]); //获取星座信息
+        if (text) str = `${text}`;
+      }
+      break;
+    case '开启防撤回':
+      reback_message_listen = true
+      str += `已开启防撤回`;
+      break;
+    case '关闭防撤回':
+      reback_message_listen = false
+      str += `已关闭防撤回`;
+      break;
+    default:
+      let check_contact_message = '', check_contact_type = ''
+      if (reback_message_listen && content.match('\" 撤回了一条消息')) {
+        for (let i = 0; i < reback_message.length; i++) {
+          if (reback_message[i].name == alias) {
+            check_contact_message = reback_message[i].content // 获取该用户最近的一条文字内容
+            check_contact_type = reback_message[i].isText
+          }
+        }
+        if (check_contact_message && check_contact_type) {
+          // 如果有返回值则进行内容回复
+          str += `太天真了，想撤回不可能的\n`;
+          str += `${alias} 说: ${check_contact_message}\n`;
+          str += `【消息防撤回仅支持文字消息，关闭防撤回请说：关闭防撤回】`;
+        }
+      } else {
+        if (reback_message.length > 29) reback_message.splice(0, 1) // 如果已记录对话超过30条，自动删除第一条
+        reback_message.push({
+          name: alias,
+          content: content,
+          isText: isText
+        })
+      }
+      break;
   }
-
-  if (room && isText) {
+  if (room) {
     // 如果是群消息 目前只处理文字消息
     const topic = await room.topic();
-    console.log(`群名: ${topic} 发消息人: ${await contact.name()} 内容: ${content}`);
-  } else if (isText) {
-    // 如果非群消息 目前只处理文字消息
-    console.log(`发消息人: ${alias} 消息内容: ${content}`);
-    if (content.substr(0, 1) == '?' || content.substr(0, 1) == '？') {
-      let contactContent = content.replace('?', '').replace('？', '');
-      if (contactContent) {
-        let res = await superagent.getRubbishType(contactContent);
-        await delay(2000);
-        await contact.say(res);
-      }
-    } else if (config.AUTOREPLY && config.AUTOREPLYPERSON.indexOf(alias) > -1) {
-      // 如果开启自动聊天且已经指定了智能聊天的对象才开启机器人聊天\
-      if (content) {
-        let reply;
-        if (config.DEFAULTBOT == '0') {
-          // 天行聊天机器人逻辑
-          reply = await superagent.getReply(content);
-          console.log('天行机器人回复：', reply);
-        } else if (config.DEFAULTBOT == '1') {
-          // 图灵聊天机器人
-          reply = await superagent.getTuLingReply(content);
-          console.log('图灵机器人回复：', reply);
-        } else if (config.DEFAULTBOT == '2') {
-          // 天行对接的图灵聊
-          reply = await superagent.getTXTLReply(content);
-          console.log('天行对接的图灵机器人回复：', reply);
-        }
-        try {
-          await delay(2000);
-          await contact.say(reply);
-        } catch (e) {
-          console.error(e);
-        }
-      }
+    // console.log(`群名: ${topic} 发消息人: ${alias} 内容: ${content}`);
+    // 如果是指定得群，就恢复指定消息
+    if (config.ROOMNAME.length > 0 && config.ROOMNAME.indexOf(topic) != -1) {
+      // 如果有返回值则进行内容回复
+      if (str) await superagent.sendRoomMessage(room, str);
     }
+  } else {
+    // console.log(`发消息人: ${alias} 消息内容: ${content}`);
+    if (str) await superagent.sendRoomMessage(contact, str)
+    // if (isText) {
+    //   // 如果非群消息 目前只处理文字消息
+    //   if (content.substr(0, 1) == '?' || content.substr(0, 1) == '？') {
+    //     let contactContent = content.replace('?', '').replace('？', '');
+    //     if (contactContent) {
+    //       let res = await superagent.getRubbishType(contactContent);
+    //       await delay(1000);
+    //     }
+    //   } else if (config.AUTOREPLY && config.AUTOREPLYPERSON.indexOf(alias) > -1) {
+    //     // 如果开启自动聊天且已经指定了智能聊天的对象才开启机器人聊天\
+    //     if (content) {
+    //       let reply;
+    //       if (config.DEFAULTBOT == '0') {
+    //         // 天行聊天机器人逻辑
+    //         reply = await superagent.getReply(content);
+    //         console.log('天行机器人回复：', reply);
+    //       } else if (config.DEFAULTBOT == '1') {
+    //         // 图灵聊天机器人
+    //         reply = await superagent.getTuLingReply(content);
+    //         console.log('图灵机器人回复：', reply);
+    //       } else if (config.DEFAULTBOT == '2') {
+    //         // 天行对接的图灵聊
+    //         reply = await superagent.getTXTLReply(content);
+    //         console.log('天行对接的图灵机器人回复：', reply);
+    //       }
+    //       try {
+    //         await delay(1000);
+    //         await contact.say(reply);
+    //       } catch (e) {
+    //         console.error(e);
+    //       }
+    //     }
+    //   }
+    // }
   }
-}
-
-// 创建微信每日说定时任务
-async function initDay() {
-  console.log(`已经设定每日说任务`);
-
-  schedule.setSchedule(config.SENDDATE, async () => {
-    console.log('你的贴心小助理开始工作啦！');
-    let logMsg;
-    let contact =
-      (await bot.Contact.find({ name: config.NICKNAME })) ||
-      (await bot.Contact.find({ alias: config.NAME })); // 获取你要发送的联系人
-    let one = await superagent.getOne(); //获取每日一句
-    let weather = await superagent.getTXweather(); //获取天气信息
-    let today = await untils.formatDate(new Date()); //获取今天的日期
-    let memorialDay = untils.getDay(config.MEMORIAL_DAY); //获取纪念日天数
-    let sweetWord = await superagent.getSweetWord();
-
-    // 你可以修改下面的 str 来自定义每日说的内容和格式
-    // PS: 如果需要插入 emoji(表情), 可访问 "https://getemoji.com/" 复制插入
-    let str = `${today}\n我们在一起的第${memorialDay}天\n\n元气满满的一天开始啦,要开心噢^_^\n\n今日天气\n${weather.weatherTips}\n${weather.todayWeather}\n每日一句:\n${one}\n\n每日土味情话：\n${sweetWord}\n\n————————最爱你的我`;
-    try {
-      logMsg = str;
-      await delay(2000);
-      await contact.say(str); // 发送消息
-    } catch (e) {
-      logMsg = e.message;
-    }
-    console.log(logMsg);
-  });
 }
 
 const bot = WechatyBuilder.build({
--- superagent/index.js
+++ superagent/index.js
@@ -1,7 +1,9 @@
 const superagent = require('./superagent');
 const config = require('../config/index');
+const schedule = require('../schedule/index');
+const untils = require('../utils/index');
 const cheerio = require('cheerio');
-const {machineIdSync} = require('node-machine-id');
+const { machineIdSync } = require('node-machine-id');
 const crypto = require('crypto');
 let md5 = crypto.createHash('md5');
 let uniqueId = md5.update(machineIdSync()).digest('hex'); // 获取机器唯一识别码并MD5，方便机器人上下文关联
@@ -9,10 +11,52 @@ const ONE = 'http://wufazhuce.com/'; // ONE的web版网站
 const TXHOST = 'http://api.tianapi.com/txapi/'; // 天行host
 const TULINGAPI = 'http://www.tuling123.com/openapi/api'; // 图灵1.0接口api
 
-async function getOne() {
-    // 获取每日一句
+// 延时函数，防止检测出类似机器人行为操作
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+// 指定对象发送消息
+async function sendRoomMessage(res, str) {
+    // 你可以修改下面的 str 来自定义每日说的内容和格式
+    // PS: 如果需要插入 emoji(表情), 可访问 "https://getemoji.com/" 复制插入
+    let logMsg;
     try {
-        let res = await superagent.req({url: ONE, method: 'GET', spider: true});
+        logMsg = str;
+        await delay(1000);
+        await res.say(str); // 发送消息
+    } catch (e) {
+        logMsg = e.message;
+    }
+    console.log(logMsg);
+}
+
+// 创建微信每日说定时任务
+async function initDay(bot) {
+    console.log(`已经设定每日说任务`);
+    schedule.setSchedule(config.SENDDATE, async () => {
+        let Rooms = config.ROOMNAME;
+        for (var i = 0; i < Rooms.length; i++) {
+            let room = await bot.Room.find({ topic: Rooms[i] }) // 获取你要发送的 - 群名称
+            // let zaoan = await getZaoAn(); //获取每日一句
+            // let one = await getOne(); //获取每日一句
+            let weather = await getTXweather(config.CITY); //获取天气信息
+            let today = await untils.formatDate(new Date()); //获取今天的日期
+            // 你可以修改下面的 str 来自定义每日说的内容和格式
+            // PS: 如果需要插入 emoji(表情), 可访问 "https://getemoji.com/" 复制插入
+            let str = `${today}\n`;
+            // str += `${zaoan}\n`
+            str += `元气满满得一天开始啦！\n`
+            str += `${weather}`
+            // str += `发送‘城市名+天气’即可获取指定城市天气信息\n`
+            // str += `每日一句:${one}\n`
+            sendRoomMessage(room, str);
+        }
+    });
+}
+
+// 获取每日一句
+async function getOne() {
+    try {
+        let res = await superagent.req({ url: ONE, method: 'GET', spider: true });
         let $ = cheerio.load(res);
         let todayOneList = $('#carousel-one .carousel-inner .item');
         let todayOne = $(todayOneList[0])
@@ -26,28 +70,208 @@ async function getOne() {
     }
 }
 
-async function getTXweather() {
+// 获取毒鸡汤
+async function getDuJiTang() {
+    let url = TXHOST + 'dujitang/';
+    try {
+        let content = await superagent.req({ url, method: 'GET', params: { key: config.TXAPIKEY } });
+        if (content.code === 200) {
+            let sweet = content.newslist[0].content;
+            let str = sweet.replace('\r\n', '<br>');
+            return str;
+        } else {
+            return '获取失败，请联系管理员'
+        }
+    } catch (err) {
+        console.log('获取接口失败', err);
+    }
+}
+
+// 获取早安心语
+async function getZaoAn() {
+    let url = TXHOST + 'zaoan/';
+    try {
+        let content = await superagent.req({ url, method: 'GET', params: { key: config.TXAPIKEY } });
+        if (content.code === 200) {
+            let sweet = content.newslist[0].content;
+            let str = sweet.replace('\r\n', '<br>');
+            return str;
+        } else {
+            return '获取失败，请联系管理员'
+        }
+    } catch (err) {
+        console.log('获取接口失败', err);
+    }
+}
+
+// 获取彩虹屁
+async function getCaiHongPi() {
+    let url = TXHOST + 'caihongpi/';
+    try {
+        let content = await superagent.req({ url, method: 'GET', params: { key: config.TXAPIKEY } });
+        if (content.code === 200) {
+            let sweet = content.newslist[0].content;
+            let str = sweet.replace('\r\n', '<br>');
+            return str;
+        } else {
+            return '获取失败，请联系管理员'
+        }
+    } catch (err) {
+        console.log('获取接口失败', err);
+    }
+}
+
+// 获取舔狗日记
+async function getTianGou() {
+    let url = TXHOST + 'tiangou/';
+    try {
+        let content = await superagent.req({ url, method: 'GET', params: { key: config.TXAPIKEY } });
+        if (content.code === 200) {
+            let sweet = content.newslist[0].content;
+            let str = sweet.replace('\r\n', '<br>');
+            return str;
+        } else {
+            return '获取失败，请联系管理员'
+        }
+    } catch (err) {
+        console.log('获取接口失败', err);
+    }
+}
+
+// 获取神回复
+async function getGodReply() {
+    let url = TXHOST + 'godreply/';
+    try {
+        let content = await superagent.req({ url, method: 'GET', params: { key: config.TXAPIKEY } });
+        if (content.code === 200) {
+            let sweet = '问：' + content.newslist[0].title + '\r\n' + '答：' + content.newslist[0].content;
+            let str = sweet.replace('\r\n', '<br>');
+            return str;
+        } else {
+            return '获取失败，请联系管理员'
+        }
+    } catch (err) {
+        console.log('获取接口失败', err);
+    }
+}
+
+// 获取土味情话
+async function getSweetWord() {
+    let url = TXHOST + 'saylove/';
+    try {
+        let content = await superagent.req({ url, method: 'GET', params: { key: config.TXAPIKEY } });
+        if (content.code === 200) {
+            let sweet = content.newslist[0].content;
+            let str = sweet.replace('\r\n', '<br>');
+            return str;
+        } else {
+            return '你很像一款游戏。我的世界'
+        }
+    } catch (err) {
+        console.log('获取接口失败', err);
+    }
+}
+
+/**
+ * @param {String} city 城市名称
+ * @param {Number} type 实时 （1）/ 天数（2，3，4，5，6，7）
+ */
+// 获取天气
+async function getTXweather(city) {
     // 获取天行天气
     let url = TXHOST + 'tianqi/';
     try {
         let content = await superagent.req({
             url, method: 'GET', params: {
                 key: config.TXAPIKEY,
-                city: config.CITY
+                city: city,
+                type: 1
             }
         });
-
+        // 10000
         if (content.code === 200) {
+            let str = ``;
             let todayInfo = content.newslist[0];
-            let obj = {
-                weatherTips: todayInfo.tips,
-                todayWeather: `今天${todayInfo.weather}\n温度:${todayInfo.lowest}/${todayInfo.highest}\n${todayInfo.wind} ${todayInfo.windspeed}\n`
-            };
-            console.info('获取天行天气成功', obj);
-            return obj;
+            str += `今天${city}${todayInfo.weather}\n`;
+            str += `温度:${todayInfo.lowest}/${todayInfo.highest}\n`;
+            str += `${todayInfo.wind} ${todayInfo.windspeed}\n`;
+            str += `${todayInfo.tips}\n`;
+            return str;
+        } else {
+            return null;
         }
     } catch (err) {
         console.log('请求天气失败', err);
+    }
+}
+
+/**
+ * @param {String} word 垃圾名称
+ */
+//获取垃圾分类结果
+async function getRubbishType(word) {
+    let url = TXHOST + 'lajifenlei/';
+    let content = await superagent.req({ url, method: 'GET', params: { key: config.TXAPIKEY, word: word } });
+
+    if (content.code === 200) {
+        let type;
+        if (content.newslist[0].type == 0) {
+            type = '是可回收垃圾';
+        } else if (content.newslist[0].type == 1) {
+            type = '是有害垃圾';
+        } else if (content.newslist[0].type == 2) {
+            type = '是厨余(湿)垃圾';
+        } else if (content.newslist[0].type == 3) {
+            type = '是其他(干)垃圾';
+        }
+        let response =
+            content.newslist[0].name +
+            type +
+            '<br>解释：' +
+            content.newslist[0].explain +
+            '<br>主要包括：' +
+            content.newslist[0].contain +
+            '<br>投放提示：' +
+            content.newslist[0].tip;
+        return response;
+    } else {
+        return '暂时还没找到这个分类信息呢';
+    }
+}
+
+// 监听新成员加入群
+async function roomListenJoin(room) {
+    if (room) {
+        room.on('join', (res, inviteeList, inviter) => {
+            let str = '';
+            str += `欢迎新人，@群主记得先发红包`;
+            sendRoomMessage(room, str);
+        })
+    }
+}
+
+// 获取星座运势
+async function getXingZuo(astro) {
+    let url = TXHOST + 'star/';
+    try {
+        let content = await superagent.req({
+            url, method: 'GET', params: {
+                key: config.TXAPIKEY,
+                astro: astro
+            }
+        });
+        if (content.code === 200) {
+            let newslist = content.newslist || [];
+            let str = '';
+            for (var i = 0; i < newslist.length; i++) {
+                str += newslist[i].type + ':' + newslist[i].content + '\n'
+            }
+            return str;
+        } else {
+            return null;
+        }
+    } catch (err) {
+        console.log('请求星座信息失败', err);
     }
 }
 
@@ -118,64 +342,21 @@ async function getReply(word) {
     }
 }
 
-async function getSweetWord() {
-    // 获取土味情话
-    let url = TXHOST + 'saylove/';
-    try {
-        let content = await superagent.req({url, method: 'GET', params: {key: config.TXAPIKEY}});
-        if (content.code === 200) {
-            let sweet = content.newslist[0].content;
-            let str = sweet.replace('\r\n', '<br>');
-            return str;
-        } else {
-            return '你很像一款游戏。我的世界'
-        }
-    } catch (err) {
-        console.log('获取接口失败', err);
-    }
-}
-
-/**
- * 获取垃圾分类结果
- * @param {String} word 垃圾名称
- */
-
-async function getRubbishType(word) {
-    let url = TXHOST + 'lajifenlei/';
-    let content = await superagent.req({url, method: 'GET', params: {key: config.TXAPIKEY, word: word}});
-    
-    if (content.code === 200) {
-        let type;
-        if (content.newslist[0].type == 0) {
-            type = '是可回收垃圾';
-        } else if (content.newslist[0].type == 1) {
-            type = '是有害垃圾';
-        } else if (content.newslist[0].type == 2) {
-            type = '是厨余(湿)垃圾';
-        } else if (content.newslist[0].type == 3) {
-            type = '是其他(干)垃圾';
-        }
-        let response =
-            content.newslist[0].name +
-            type +
-            '<br>解释：' +
-            content.newslist[0].explain +
-            '<br>主要包括：' +
-            content.newslist[0].contain +
-            '<br>投放提示：' +
-            content.newslist[0].tip;
-        return response;
-    } else {
-        return '暂时还没找到这个分类信息呢';
-    }
-}
-
 module.exports = {
+    sendRoomMessage,
+    initDay,
     getOne,
+    getZaoAn,
+    getCaiHongPi,
     getTXweather,
+    getXingZuo,
+    getTianGou,
     getReply,
+    getDuJiTang,
+    getGodReply,
     getSweetWord,
     getTuLingReply,
     getTXTLReply,
-    getRubbishType
+    getRubbishType,
+    roomListenJoin
 };
